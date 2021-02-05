ef1cba926d85124241072b686dc0bafbfa934b24
richardthorp <richardthorp@hotmail.co.uk>
2021-02-05 13:07:41 +0000

    Fix search bar results pagination bug)

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_pymongo import PyMongo
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
+from bson.objectid import ObjectId
 from flask_paginate import Pagination, get_page_args
 if os.path.exists("env.py"):
     import env
@@ -11,6 +12,7 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
@@ -22,31 +24,54 @@ mongo.db.restaurants.create_index([("name", "text"),("cuisine", "text")])
 
 @app.route("/", methods=["GET", "POST"])
 def search():
-    if request.method == "POST":
-        search_term = request.form.get("search")
-        results = mongo.db.restaurants.find({"$text":
-                                            {"$search": search_term}})
-        page, per_page, offset = get_page_args(page_parameter='page',
-                                           per_page_parameter='per_page')
-        per_page = 9
-        offset = page * per_page
-        total = results.count()
-        paginated_results = results[offset: offset + per_page]
-        pagination = Pagination(page=page, per_page=per_page, total=total,
-                            css_framework='bootstrap4')
-        return render_template("result.html",
-                               results=paginated_results,
-                               page=page,
-                               per_page=per_page,
-                               pagination=pagination,)
     return render_template("search.html")
+
+
+@app.route("/searchbar_results", methods=["GET", "POST"])
+def searchbar_results():
+    if request.method == "POST":
+        session["search_term"] = request.form.get("search")
+
+    results = mongo.db.restaurants.find({"$text":
+                                        {"$search": session["search_term"]}})
+
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+    per_page = 9
+    offset = page * per_page
+    total = results.count()
+    paginated_results = results[offset: offset + per_page]
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template("result.html",
+                           results=paginated_results,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination)
+
+    results = list(session["results"])
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+    per_page = 9
+    offset = page * per_page
+    total = results.count()
+    paginated_results = results[offset: offset + per_page]
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template("result.html",
+                           results=paginated_results,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination)
 
 
 @app.route("/result/<keyword>")
 def keyword_search(keyword):
-    results = mongo.db.restaurants.find({"$text": {"$search": keyword}})
+    results = mongo.db.restaurants.find({"$text":
+                                            {"$search": keyword}})
     page, per_page, offset = get_page_args(page_parameter='page',
                                            per_page_parameter='per_page')
+
     # If you are hard coding the number of items per page
     # then uncomment the two lines below
     per_page = 9
@@ -55,8 +80,6 @@ def keyword_search(keyword):
     # Gets the total values to be used later
     total = results.count()
 
-    # Gets all the values
-    # thetests = mongo.db.mongoTestingDataBase.find()
     # Paginates the values
     paginated_results = results[offset: offset + per_page]
 
@@ -68,11 +91,6 @@ def keyword_search(keyword):
                            per_page=per_page,
                            pagination=pagination,
                            )
-
-
-@app.route("/result")
-def result():
-    return render_template("result.html")
 
 
 if __name__ == "__main__":
